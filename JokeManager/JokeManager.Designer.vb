bb9298890b08ff304a2b99094891327b816ef81f
Hamza Akiour <akiourh@gmail.com>
2017-04-02 15:52:42 +0200

    Add files via upload

---
+++ JokeManager/JokeManager.Designer.vb
@@ -0,0 +1,104 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class JokeManager
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(JokeManager))
+        Me.btnblocksys = New System.Windows.Forms.Button()
+        Me.butpoweroff = New System.Windows.Forms.Button()
+        Me.btnscream = New System.Windows.Forms.Button()
+        Me.preventiontimer = New System.Windows.Forms.Timer(Me.components)
+        Me.AxWindowsMediaPlayer1 = New AxWMPLib.AxWindowsMediaPlayer()
+        CType(Me.AxWindowsMediaPlayer1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnblocksys
+        '
+        Me.btnblocksys.FlatAppearance.BorderSize = 3
+        Me.btnblocksys.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnblocksys.Location = New System.Drawing.Point(25, 12)
+        Me.btnblocksys.Name = "btnblocksys"
+        Me.btnblocksys.Size = New System.Drawing.Size(172, 35)
+        Me.btnblocksys.TabIndex = 0
+        Me.btnblocksys.Text = "BLOCK SYSTEM JOKE"
+        Me.btnblocksys.UseVisualStyleBackColor = True
+        '
+        'butpoweroff
+        '
+        Me.butpoweroff.FlatAppearance.BorderSize = 3
+        Me.butpoweroff.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.butpoweroff.Location = New System.Drawing.Point(25, 53)
+        Me.butpoweroff.Name = "butpoweroff"
+        Me.butpoweroff.Size = New System.Drawing.Size(171, 35)
+        Me.butpoweroff.TabIndex = 1
+        Me.butpoweroff.Text = "SHUTDOWN TIMER"
+        Me.butpoweroff.UseVisualStyleBackColor = True
+        '
+        'btnscream
+        '
+        Me.btnscream.FlatAppearance.BorderSize = 3
+        Me.btnscream.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnscream.Location = New System.Drawing.Point(25, 94)
+        Me.btnscream.Name = "btnscream"
+        Me.btnscream.Size = New System.Drawing.Size(171, 35)
+        Me.btnscream.TabIndex = 2
+        Me.btnscream.Text = "SCREAM JOKE TIMER"
+        Me.btnscream.UseVisualStyleBackColor = True
+        '
+        'preventiontimer
+        '
+        Me.preventiontimer.Interval = 60000
+        '
+        'AxWindowsMediaPlayer1
+        '
+        Me.AxWindowsMediaPlayer1.Enabled = True
+        Me.AxWindowsMediaPlayer1.Location = New System.Drawing.Point(40, 261)
+        Me.AxWindowsMediaPlayer1.Name = "AxWindowsMediaPlayer1"
+        Me.AxWindowsMediaPlayer1.OcxState = CType(resources.GetObject("AxWindowsMediaPlayer1.OcxState"), System.Windows.Forms.AxHost.State)
+        Me.AxWindowsMediaPlayer1.Size = New System.Drawing.Size(130, 45)
+        Me.AxWindowsMediaPlayer1.TabIndex = 3
+        Me.AxWindowsMediaPlayer1.Visible = False
+        '
+        'JokeManager
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(225, 141)
+        Me.Controls.Add(Me.AxWindowsMediaPlayer1)
+        Me.Controls.Add(Me.btnscream)
+        Me.Controls.Add(Me.butpoweroff)
+        Me.Controls.Add(Me.btnblocksys)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.Name = "JokeManager"
+        Me.Text = "Joke Manager"
+        CType(Me.AxWindowsMediaPlayer1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents btnblocksys As Button
+    Friend WithEvents butpoweroff As Button
+    Friend WithEvents btnscream As Button
+    Friend WithEvents AxWindowsMediaPlayer1 As AxWMPLib.AxWindowsMediaPlayer
+    Friend WithEvents preventiontimer As Timer
+End Class
---
+++ JokeManager/JokeManager.vb
@@ -0,0 +1,136 @@
+﻿'DEVELOPED BY HAMZA AKIOUR (HTTPS://WWW.HAMZA.ES)
+Imports System.Runtime.InteropServices
+Public Class JokeManager
+
+    Private Sub btnblocksys_Click(sender As Object, e As EventArgs) Handles btnblocksys.Click
+        screenfake()
+    End Sub
+
+    Private Sub butpoweroff_Click(sender As Object, e As EventArgs) Handles butpoweroff.Click
+        poweroff()
+    End Sub
+
+    Private Sub btnscream_Click(sender As Object, e As EventArgs) Handles btnscream.Click
+        screamfake()
+    End Sub
+
+    Sub screenfake()
+        'SCREEN FAKE: LAUNCH A SCREENSHOT OF THE ACTUAL STATE OF MACHINE (ON FULLSCREEN)...
+        Me.Visible = False
+        'Making route path of desktop
+        Dim path As String = My.Computer.FileSystem.SpecialDirectories.Desktop
+
+        'Doing the screenshot
+        'Only take screenshot of primary screen
+        Me.Visible = False
+        Threading.Thread.Sleep(500)
+        Dim screenshot As New System.Drawing.Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height, System.Drawing.Imaging.PixelFormat.Format32bppRgb)
+        Dim graph As Graphics = Graphics.FromImage(screenshot)
+        graph.CopyFromScreen(0, 0, 0, 0, screenshot.Size)
+
+        'Make new form whit actual screen as background
+        fullscreen(screenshot)
+        'Exit app on 1 min
+        preventiontimer.Enabled = True
+    End Sub
+
+    Sub fullscreen(thisscreen)
+        'DO A FULLSCREEN WHIT THE IMAGE RECEIVED (ON VAR "thisscreen")
+        Dim DesktopForm As New Form
+
+        'Set size of form (full screen size)
+        DesktopForm.Size = New Size(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height)
+        'Put the image as background
+        DesktopForm.BackgroundImage = thisscreen
+        'show the form on screen
+        DesktopForm.Show()
+
+        'Quit the borders on form
+        DesktopForm.FormBorderStyle = DesktopForm.FormBorderStyle.None
+        'Maximize the new form
+        'First minimized, and then force maximized (if only maximized, when user have minimized the program, not work)
+        DesktopForm.WindowState = FormWindowState.Minimized
+        DesktopForm.WindowState = FormWindowState.Maximized
+    End Sub
+
+    Sub poweroff()
+        'POWER OFF THE COMPUTER: POWER OFF THE MACHINE...
+
+        'ask time for countdown
+        Dim timeshutd As String = InputBox("Countdown (in sec)", "It's the time ;)", "0").Trim
+        'No value = canceled
+        If timeshutd = vbNullString Then
+            'Aborted
+        Else
+            Me.Visible = False
+            'if is numeric and not negative number
+            If IsNumeric(timeshutd) And timeshutd >= 0 Then
+                Process.Start("cmd", "/c shutdown -s -c "" "" -t " & timeshutd)
+            Else
+                MessageBox.Show("Omg!, ¿You don't understand what are sec? ¡Seconds!", "Stupid...")
+            End If
+            'Exit app on 1 min
+            preventiontimer.Enabled = True
+        End If
+    End Sub
+
+    Sub screamfake()
+        'SCREAM FAKE: LAUNCH A ONE IMAGE + PLAY A SOUND FOR SCARE USER
+
+        'ask time for countdown
+        Dim timeshutd As String = InputBox("Countdown (in sec)", "It's the time ;)", "0").Trim
+        'No value = canceled
+        If timeshutd = vbNullString Then
+            'Aborted
+        Else
+            Me.Visible = False
+            'Do the countdown
+            Threading.Thread.Sleep(timeshutd * 1000)
+            'UP volume of sistem, do that function for 99 times for sure te new volume is 100%
+            For i = 0 To 99
+                SendMessage(Me.Handle, WM_APPCOMMAND, &H30292, APPCOMMAND_VOLUME_UP * &H10000)
+            Next
+            'TRY to play audio (3 diferents ways for make sure)
+            Try
+                My.Computer.Audio.Play(My.Resources.scream, AudioPlayMode.WaitToComplete)
+            Catch ex As Exception
+                Try
+                    AxWindowsMediaPlayer1.URL = CurDir() & "\scream.wav"
+                    'AxWindowsMediaPlayer1.Ctlcontrols.play()
+                Catch ax As Exception
+                    Try
+                        My.Computer.Audio.Play(CurDir() & "\scream.wav", AudioPlayMode.WaitToComplete)
+                    Catch ix As Exception
+                    End Try
+                End Try
+            End Try
+            'Get one number random (1 to 3), and then select a terror image
+            Randomize()
+            Dim value As Integer = CInt(Int((3 * Rnd()) + 1))
+            Select Case value
+                Case "1"
+                    fullscreen(My.Resources.Resources.terror1)
+                Case "2"
+                    fullscreen(My.Resources.Resources.terror2)
+                Case "3"
+                    fullscreen(My.Resources.Resources.terror3)
+            End Select
+            'Exit app on 1 min
+            preventiontimer.Enabled = True
+        End If
+    End Sub
+    'VOLUME FUNCTION, NOT DEVELOPED BY ME
+    <DllImport("user32.dll", SetLastError:=True, CharSet:=CharSet.Auto)> Private Shared Function SendMessage(ByVal hWnd As IntPtr, ByVal Msg As UInteger, ByVal wParam As IntPtr, ByVal lParam As IntPtr) As IntPtr
+    End Function
+    Const WM_APPCOMMAND As UInteger = &H319
+    Const APPCOMMAND_VOLUME_UP As UInteger = &HA
+
+    Private Sub preventiontimer_Tick(sender As Object, e As EventArgs) Handles preventiontimer.Tick
+        Application.Exit()
+    End Sub
+
+    Private Sub JokeManager_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
+
